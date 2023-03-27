5c851a3b0a00ed73c38aabb93bb495d16dd2be9c
Vinicius Lazzari <vinilazzari028@gmail.com>
2023-03-27 19:17:37 -0300

    ✨ Blinn Phong lightning

--- src/shaders/shader_fragment.glsl
+++ src/shaders/shader_fragment.glsl
@@ -19,25 +19,30 @@ void main()
 
     vec4 p = position_world;
     vec4 n = normalize(normal);
+
+    // Vetor que define o sentido da fonte de luz em relação ao ponto atual.
     vec4 l = normalize(vec4(1.0,1.0,0.5,0.0));
+
+    // Vetor que define o sentido da câmera em relação ao ponto atual.
     vec4 v = normalize(camera_position - p);
-    vec4 r = -l + 2 * n * dot(n, l);
+
+    vec4 halfway = normalize(l + v);
 
     vec3 Kd; // Refletância difusa
     vec3 Ks; // Refletância especular
     vec3 Ka; // Refletância ambiente
     float q; // Expoente especular para o modelo de iluminação de Phong
 
-    Kd = vec3(0.8,0.4,0.08);
+    Kd = vec3(0.5,0.4,0.08);
     Ks = vec3(0.0,0.0,0.0);
     Ka = vec3(0.4,0.2,0.04);
-    q = 1.0;
+    q = 2.0;
 
     // Espectro da fonte de iluminação
     vec3 I = vec3(1.0,1.0,1.0);
 
     // Espectro da luz ambiente
-    vec3 Ia = vec3(0.2,0.2,0.2);
+    vec3 Ia = vec3(0.5,0.5,0.5);
 
     // Termo difuso utilizando a lei dos cossenos de Lambert
     vec3 lambert_diffuse_term = Kd * I * max(0, dot(n, l));
@@ -46,7 +51,7 @@ void main()
     vec3 ambient_term = Ka * Ia;
 
     // Termo especular utilizando o modelo de iluminação de Phong
-    vec3 phong_specular_term = Ks * I * pow(max(0, dot(r, v)), q); // PREENCHA AQUI o termo especular de Phong
+    vec3 phong_specular_term = Ks * I * pow(max(0, dot(n, halfway)), q);
 
     vec3 texture_color = texture(sampler, texture_coords).xyz;
 
